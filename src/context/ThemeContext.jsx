c9a0b7a48f8d1b47885446cec5de37696a5cce8b
Umesh Rajbanshi <ums.rbc07@gmail.com>
2026-05-25 13:14:26 +0545

    dark as default theme

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState('dark');
 
     useEffect(() => {
         const root = window.document.documentElement;
